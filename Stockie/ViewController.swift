efa24258a2ba822121fc5b5b9c0e1a9a7ff8d930
Condo97 <acoundou@gmail.com>
2020-02-04 22:48:51 -0500

    Initial Commit

---
+++ Stockie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stockie
+//
+//  Created by Alex Coundouriotis on 2/4/20.
+//  Copyright © 2020 Alex Coundouriotis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
