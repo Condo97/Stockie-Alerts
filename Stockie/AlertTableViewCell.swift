77b553a4148f4f3a94203ab072d68278517e19cc
Condo97 <acoundou@gmail.com>
2020-02-25 15:48:31 -0500

    Initial Commit and Push

---
+++ Stockie/AlertTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  AlertTableViewCell.swift
+//  Stockie
+//
+//  Created by Alex Coundouriotis on 2/24/20.
+//  Copyright © 2020 Alex Coundouriotis. All rights reserved.
+//
+
+import UIKit
+
+protocol AlertTableViewCellDelegate {
+    func didPressArrowButton()
+}
+
+class AlertTableViewCell: UITableViewCell {
+    @IBOutlet weak var symbol: UILabel!
+    @IBOutlet weak var currentPrice: UILabel!
+    @IBOutlet weak var stockImageView: UIImageView!
+    @IBOutlet weak var alertPrice: UILabel!
+    @IBOutlet weak var bellImageView: UIImageView!
+    @IBOutlet weak var arrowImageView: UIImageView!
+    @IBOutlet weak var arrowButton: UIButton!
+    
+    var cellDelegate : AlertTableViewCellDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func arrowButton(_ sender: Any) {
+        cellDelegate?.didPressArrowButton()
+    }
+}
--- Stockie/AppDelegate.swift
+++ Stockie/AppDelegate.swift
@@ -11,11 +11,12 @@ import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        registerForPushNotifications()
+        
         return true
     }
 
@@ -77,6 +78,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
+    
+    func registerForPushNotifications() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge, .carPlay]) {
+            [weak self] granted, error in
+            
+            print("Permission granted: \(granted)")
+            guard granted else { return } //Guard essentially is like "if (granted) else { return }
+            self?.getNotificationSettings()
+        }
+    }
+    
+    func getNotificationSettings() {
+        UNUserNotificationCenter.current().getNotificationSettings {
+            settings in
+            
+            print("Notification settings: \(settings)")
+            
+            guard settings.authorizationStatus == .authorized else { return }
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let token = tokenParts.joined()
+        print("Device token: \(token)")
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register: \(error)")
+    }
 }
 
