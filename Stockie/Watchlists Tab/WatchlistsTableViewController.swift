51a120afea92a6b9b39bffc51f04fbfe997f6672
Condo97 <acoundou@gmail.com>
2020-03-27 22:16:45 -0400

    NOT STABLE but before some changes :)

--- Stockie/Watchlists Tab/WatchlistsTableViewController.swift
+++ Stockie/Watchlists Tab/WatchlistsTableViewController.swift
@@ -67,83 +67,7 @@ class WatchlistsTableViewController: UITableViewController {
                 }
             }
             
-//            var additionalSections = 0
-//            for tempWatchlist in tempWatchlists {
-//                if !watchlists.contains(tempWatchlist) {
-//                    additionalSections += 1
-//                    watchlists.append(tempWatchlist)
-//                }
-//            }
-//
-//            for i in 0..<watchlists.count {
-//                let watchlist = watchlists[i]
-//                if !tempWatchlists.contains(watchlist) {
-//                    additionalSections -= 1
-//                                          watchlists.remove(at: i)
-//                }
-//            }
-            
-//            var additional : [Int] = []
-//            for i in 0..<watchlists.count {
-//                let watchlist = watchlists[i]
-//                var additionalRows = 0
-//
-//                var found = false
-//                for j in 0..<tempWatchlists.count {
-//                    let tempWatchlist = tempWatchlists[j]
-//
-//                    if watchlist.watchlistID == tempWatchlist.watchlistID {
-//                        //If Stock count of watchlist is > Stock count of tempWatchlist, that means stocks have been deleted from CoreData. Decrement rows in section.
-//                        if watchlist.stocks.count > tempWatchlist.stocks.count { additionalRows -= 1 }
-//
-//                        //If Stock count of tempWatchlist is > Stock count of Watchlist, that means stocks have been added to CoreData. Increment rows in section.
-//                        else if watchlist.stocks.count < tempWatchlist.stocks.count { additionalRows += 1 }
-//
-//                        found = true
-//                    }
-//                }
-//
-//                if found {
-//                    additional.append(additionalRows)
-//                }
-//            }
-            
             self.watchlists = tempWatchlists
-//
-//            if tempWatchlists.count > watchlists.count {
-//                for i in 0..<(tempWatchlists.count - watchlists.count) {
-//                    tableView.insertSections(IndexSet(integer: i), with: .automatic)
-//                }
-//            } else if tempWatchlists.count < watchlists.count {
-//                for i in 0..<(watchlists.count - tempWatchlists.count) {
-//                    tableView.deleteSections(IndexSet(integer: i), with: .automatic)
-//                }
-//            }
-//
-//            var index = 0
-//            for _ in 0..<watchlists.count {
-//                let watchlist = watchlists[index]
-//
-//                var found = false
-//                for i in 0..<tempWatchlists.count {
-//                    if watchlist.watchlistID == tempWatchlists[i].watchlistID { found = true }
-//                }
-//
-//                tableView.deleteRows(at: [IndexPath(row: index, section: index)], with: .automatic)
-//                watchlists.remove(at: watchlists.firstIndex(of: watchlist)!)
-//            }
-//
-//
-//            index = 0
-//            for i in 0..<tempWatchlists.count {
-//                let tempWatchlist = tempWatchlists[i]
-//                if !watchlists.contains(tempWatchlist) {
-//                    tableView.insertRows(at: [IndexPath(row: index, section: i)], with: .automatic)
-//                    watchlists.append(tempWatchlist)
-//                } else { index += 1 }
-//            }
-            
-            
             
             tableView.endUpdates()
             tableView.reloadData()
@@ -177,18 +101,23 @@ class WatchlistsTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
+        if watchlists.count == 0 { return 1 }
         return watchlists.count
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if watchlists.count == 0 { return "" }
         return watchlists[section].name
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if watchlists.count == 0 { return 1 }
         return watchlists[section].stocks.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if watchlists.count == 0 { return tableView.dequeueReusableCell(withIdentifier: "noWatchlistsCell")! }
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "watchlistCell") as! WatchlistTableViewCell
         
         cell.symbolLabel.text = watchlists[indexPath.section].stocks[indexPath.row].symbol
